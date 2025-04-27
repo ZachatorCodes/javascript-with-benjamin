d1e496c31793362be310807c00ddb5ad1240c5b6
Zach Schwartz <zachatorcodes@gmail.com>
2025-04-27 15:37:24 -0600

    Push warmup code

---
+++ session-3/index.js
@@ -0,0 +1,15 @@
+// WARMUP
+// Create a constant variable for your favorite sports team
+// Example: keyword variableName = data
+const benFavTeam = "bostonCeltics";
+// Create a changeable variable for your age
+let benAge = 12;
+// Reassign your age
+// Example: variableName = data
+benAge = 12;
+// Print your team and age to the console
+console.log(benFavTeam);
+console.log(benAge);
+
+/////////////////////////////////////////////////
+
